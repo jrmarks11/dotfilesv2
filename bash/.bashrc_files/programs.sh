7d07c28098f35172e7bdce61c6b77657143f126e
John Marks <jmarks@covermymeds.com>
2017-11-05 15:59:00 -0500

    Move bashrc contents to appropriate files

---
+++ bash/.bashrc_files/programs.sh
@@ -0,0 +1,21 @@
+[ -f ~/.asdf/asdf.sh ] && . ~/.asdf/asdf.sh
+
+command -v brew >/dev/null 2>&1 &&
+  [[ -f $(brew --prefix)/etc/bash_completion ]] &&
+  { . $(brew --prefix)/etc/bash_completion; }
+
+[ -f /usr/local/opt/chruby/share/chruby/chruby.sh ] && . /usr/local/opt/chruby/share/chruby/chruby.sh
+command -v chruby >/dev/null 2>&1 && [[ $(hostname -s) =~ ^CMM ]] && { chruby 2.2.2; }
+
+if [[ $(command -v fasd) ]]; then
+  fasd_cache="$HOME/.fasd-init-bash"
+  if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+    fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >| "$fasd_cache"
+  fi
+  source "$fasd_cache"
+  unset fasd_cache
+fi
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+command -v hub >/dev/null 2>&1 && { eval "$(hub alias -s)"; }
---
+++ bash/.bashrc_files/settings.sh
@@ -0,0 +1,26 @@
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
+
+if [ -d "$HOME/bin" ] ; then
+  PATH="$PATH:$HOME/bin"
+fi
+
+if [ -d "$HOME/dbrady-bin" ] ; then
+  PATH="$PATH:$HOME/dbrady-bin"
+fi
+
+export PATH="/usr/local/sbin:$PATH"
+
+if [ -z "$SSH_AUTH_SOCK" ] ; then
+  eval `ssh-agent -s`
+  ssh-add
+fi
+
+export HISTFILESIZE=10000
+export HISTSIZE=10000
+export HISTCONTROL=ignoreboth:erasedups
+shopt -s histappend
+stty -ixon
+export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+
+export VISUAL=vim
+export EDITOR="$VISUAL"
