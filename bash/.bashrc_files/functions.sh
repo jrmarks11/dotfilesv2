9d77be9cbf787da44afa602eb0663ecbb261635a
John Marks <jmarks@covermymeds.com>
2017-12-17 20:33:37 -0500

    New tmux function which basicly does all the things

--- bash/.bashrc_files/functions.sh
+++ bash/.bashrc_files/functions.sh
@@ -26,9 +26,10 @@ gb() {
   fi
 }
 
-fs() {
-  local session
-  session=$(tmux list-sessions -F "#{session_name}" | \
-    fzf --height 40% --reverse --query="$1" --select-1 --exit-0) &&
-    tmux switch-client -t "$session"
+tm() {
+  [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
+  if [ $1 ]; then
+     tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
+  fi
+  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
 }
