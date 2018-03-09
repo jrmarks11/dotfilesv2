43f77f6f340f07b2e6955c7fa86f6ba2131906de
John Marks <jmarks@covermymeds.com>
2018-03-09 05:27:00 -0500

    Using fd instead of rg for fzf

--- bash/.bashrc_files/settings.sh
+++ bash/.bashrc_files/settings.sh
@@ -1,4 +1,5 @@
-export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_CTRL_T_COMMAND='fd --type f --type d --hidden --follow --exclude .git'
 export FZF_DEFAULT_OPTS='--reverse --border'
 
 if [ -d "$HOME/bin" ] ; then
@@ -22,8 +23,9 @@ export HISTSIZE=100000
 IGNORE_LIST='&:[ ]*:bg:cd:cd *:clear:exit:fg:ga:gb:gc *:gco *:gd:gdc:gdl:gdm:gh'
 IGNORE_LIST+=':gn *:gs:gu:gull:gush:history:j *:jj *:ls:ls -la:tm:v *:vim:'
 export HISTIGNORE=$IGNORE_LIST
-shopt -s histappend
 shopt -s checkwinsize
+shopt -s cmdhist
+shopt -s histappend
 stty -ixon
 
 PROMPT_DIRTRIM=2
