27bd672b97dcb6d2134554c5daf406349b5ce137
John Marks <jrmarks11@gmail.com>
2018-04-07 09:22:41 -0400

    Setup asdf plugins

---
+++ macos/asdf_setup.sh
@@ -0,0 +1,3 @@
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir
+asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby
