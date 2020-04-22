f2df5276102b76dc373a6325eb9e7b2108917d85
John Marks <jrmarks11@gmail.com>
2020-04-22 06:22:05 -0500

    Simplify startup

--- macos/startup.sh
+++ macos/startup.sh
@@ -6,18 +6,11 @@ echo "Running ./macos/new_comp.sh..."
 echo "Running ./macos/brew.sh..."
 ~/dotfilesv2/macos/brew.sh
 
-echo "Running ./macos/asdf_setup.sh..."
-~/dotfilesv2/macos/asdf_setup.sh
-
 pushd ~/dotfilesv2 || return
 pwd
 
 echo "Running stow -R config..."
 stow -R config
-echo "Running stow -R ctags..."
-stow -R ctags
-echo "Running stow -R git..."
-stow -R scripts
 echo "Running stow -R tmux..."
 stow -R tmux
 echo "Running stow -R vim..."
