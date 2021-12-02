080db6acdec9d5163650404cc1cf04f799513ae2
John Marks <jrmarks11@gmail.com>
2021-12-02 11:00:56 -0600

    Remove unused keybinding

--- zsh/.zsh-files/keybindings.zsh
+++ zsh/.zsh-files/keybindings.zsh
@@ -8,6 +8,7 @@ bindkey "^[f" forward-word
 bindkey "^a" beginning-of-line
 bindkey "^e" end-of-line
 bindkey "^k" kill-line
+
 bindkey "^n" history-substring-search-down
 bindkey "^p" history-substring-search-up
 
@@ -17,14 +18,6 @@ backward-kill-dir () {
 }
 zle -N backward-kill-dir
 bindkey '^[^?' backward-kill-dir
-
-# Copy the most recent command to the clipboard
-function _pbcopy_last_command(){
-  history | tail -1 | sed 's/ *[0-9]* *//' | pbcopy && \
-    tmux display-message "Previous command coppied to clipboard"
-}
-zle -N pbcopy-last-command _pbcopy_last_command
-bindkey '^y' pbcopy-last-command
 
 autoload -U edit-command-line
 zle -N edit-command-line
