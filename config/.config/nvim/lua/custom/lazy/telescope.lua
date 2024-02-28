59e0eb200db49efd1f8149bbf30c3d49aa42353d
John Marks <jrmarks11@gmail.com>
2024-02-28 05:19:04 -0600

    Use a plugin for MRU

--- config/.config/nvim/lua/custom/lazy/telescope.lua
+++ config/.config/nvim/lua/custom/lazy/telescope.lua
@@ -1,14 +1,12 @@
 return {
   {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'make'
-  },
-  {
     'nvim-telescope/telescope.nvim',
     tag = "0.1.5",
 
     dependencies = {
       'nvim-lua/plenary.nvim',
+      'smartpde/telescope-recent-files',
+      'nvim-telescope/telescope-fzf-native.nvim', build = 'make'
     },
 
     config = function()
@@ -23,11 +21,21 @@ return {
               preview_cutoff = 40,
               vertical = { width = 0.9, height = 0.9, preview_height = 0.6 },
             },
-          }
+          },
+          extensions = {
+            recent_files = {
+              only_cwd = true,
+            },
+          },
+          pickers = {
+            find_files = {
+              find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+            },
+          },
         })
-      telescope.load_extension("fzf")
 
-      local tutil = require('util.telescope-commands')
+      telescope.load_extension("fzf")
+      telescope.load_extension("recent_files")
 
       map("n", "<Space>d", function() builtin.find_files({ cwd = "%:h" }) end)
       map('n', '<Space>f', function()
@@ -39,7 +47,9 @@ return {
       map("n", "<Space>j", function()
         builtin.grep_string({ search = vim.fn.input("grep ❯ ") })
       end)
-      map("n", "<Space>r", tutil.git_recent_files)
+
+      local recent_files = require('telescope').extensions.recent_files
+      map("n", "<Space>r", function()recent_files.pick()end)
       map('n', '<Space>t', builtin.find_files)
       map("n", "<Space>g", builtin.git_status)
 
