6cccf4f999f4dbe1a558d76d9c38af61abb50742
John Marks <jrmarks11@gmail.com>
2024-02-29 14:41:49 -0600

    Return to confirm completion

--- config/.config/nvim/lua/custom/lazy/lsp.lua
+++ config/.config/nvim/lua/custom/lazy/lsp.lua
@@ -77,8 +77,7 @@ return {
       mapping = cmp.mapping.preset.insert({
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<return>'] = cmp.mapping.confirm({ select = true }),
       }),
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
