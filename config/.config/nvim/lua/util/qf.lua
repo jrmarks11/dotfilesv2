084c1d4798ec0217dc6e1ce5ba0ac99d6c3f2203
John Marks <jrmarks11@gmail.com>
2024-03-01 09:07:51 -0600

    Refactoring qf

--- config/.config/nvim/lua/util/qf.lua
+++ config/.config/nvim/lua/util/qf.lua
@@ -1,39 +1,42 @@
 local M = {}
 
+local function vim_cmd(command, backup_command)
+  local cmd = vim.cmd
+  local success, err = pcall(cmd, command)
+
+  if not success and backup_command then
+    local backup_success, backup_err = pcall(cmd, backup_command)
+    if not backup_success then
+      print(backup_err)
+    end
+  elseif not success then
+    print(err)
+  end
+end
+
 function M.safe_next(prefix)
   local next_cmd = prefix .. 'next'
   local first_cmd = prefix .. 'first'
-  local success, _ = pcall(vim.cmd, next_cmd)
-  if not success then
-    vim.cmd(first_cmd)
-  end
+  vim_cmd(next_cmd, first_cmd)
 end
 
 function M.safe_prev(prefix)
   local prev_cmd = prefix .. 'prev'
   local last_cmd = prefix .. 'last'
-  local success, _ = pcall(vim.cmd, prev_cmd)
-  if not success then
-    vim.cmd(last_cmd)
-  end
+  vim_cmd(prev_cmd, last_cmd)
 end
 
 function M.toggle_list(list_type)
   local actions = {
     c = {
       check = function(win) return win.quickfix == 1 end,
-      open = function() vim.cmd('copen') end,
-      close = function() vim.cmd('cclose') end,
+      open = function() vim_cmd('copen') end,
+      close = function() vim_cmd('cclose') end,
     },
     l = {
       check = function(win) return win.loclist == 1 end,
-      open = function()
-        local status, err = pcall(vim.cmd, 'lopen')
-        if not status then
-          print(err)
-        end
-      end,
-      close = function() vim.cmd('lclose') end,
+      open = function() vim_cmd('lopen') end,
+      close = function() vim_cmd('lclose') end,
     }
   }
 
