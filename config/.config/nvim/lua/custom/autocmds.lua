fc602d06330fba7cf76a88cc38a77a852862df61
John Marks <jrmarks11@gmail.com>
2024-03-01 09:38:15 -0600

    Use autocmd to open last file instead of zsh

--- config/.config/nvim/lua/custom/autocmds.lua
+++ config/.config/nvim/lua/custom/autocmds.lua
@@ -1,67 +1,81 @@
-local auto_resize_group = vim.api.nvim_create_augroup('AutoResize', { clear = true })
-vim.api.nvim_create_autocmd('VimResized', {
-    group = auto_resize_group,
-    pattern = '*',
-    command = 'wincmd =',
-  })
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
 
-local auto_save_and_read_group = vim.api.nvim_create_augroup('AutoSaveAndRead', { clear = true })
-vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged' }, {
-    group = auto_save_and_read_group,
-    pattern = '*',
-    callback = function()
-      vim.cmd('silent! %s/\\s\\+$//e')
-      vim.cmd('silent! wall')
-    end,
-  })
-vim.api.nvim_create_autocmd('CursorHold', {
-    group = auto_save_and_read_group,
-    pattern = '*',
-    command = 'silent! checktime',
-  })
+local auto_resize_group = augroup('AutoResize', { clear = true })
+autocmd('VimResized', {
+  group = auto_resize_group,
+  pattern = '*',
+  command = 'wincmd =',
+})
 
-local cursor_line_group = vim.api.nvim_create_augroup('CursorLine', { clear = true })
-vim.api.nvim_create_autocmd({ 'WinEnter', 'InsertLeave' }, {
-    group = cursor_line_group,
-    pattern = '*',
-    command = 'set cursorline',
-  })
-vim.api.nvim_create_autocmd({ 'WinLeave', 'InsertEnter' }, {
-    group = cursor_line_group,
-    pattern = '*',
-    command = 'set nocursorline',
-  })
+local auto_save_and_read_group = augroup('AutoSaveAndRead', { clear = true })
+autocmd({ 'InsertLeave', 'TextChanged' }, {
+  group = auto_save_and_read_group,
+  pattern = '*',
+  callback = function()
+    vim.cmd('silent! %s/\\s\\+$//e')
+    vim.cmd('silent! wall')
+  end,
+})
+autocmd('CursorHold', {
+  group = auto_save_and_read_group,
+  pattern = '*',
+  command = 'silent! checktime',
+})
 
-local ensure_directory_exists_group = vim.api.nvim_create_augroup('EnsureDirectoryExists', { clear = true })
-vim.api.nvim_create_autocmd('BufNewFile', {
-    group = ensure_directory_exists_group,
-    pattern = '*',
-    callback = function()
-      require('util.ensure_directory_exists').ensure_directory_exists()
-    end,
-  })
+local cursor_line_group = augroup('CursorLine', { clear = true })
+autocmd({ 'WinEnter', 'InsertLeave' }, {
+  group = cursor_line_group,
+  pattern = '*',
+  command = 'set cursorline',
+})
+autocmd({ 'WinLeave', 'InsertEnter' }, {
+  group = cursor_line_group,
+  pattern = '*',
+  command = 'set nocursorline',
+})
 
-local fast_escape_group = vim.api.nvim_create_augroup('FastEscape', { clear = true })
-vim.api.nvim_create_autocmd({ 'InsertEnter', 'CmdlineEnter' }, {
-    group = fast_escape_group,
-    pattern = '*',
-    command = 'set timeoutlen=200',
-  })
-vim.api.nvim_create_autocmd({ 'InsertLeave', 'CmdlineLeave' }, {
-    group = fast_escape_group,
-    pattern = '*',
-    command = 'set timeoutlen=1000',
-  })
+local ensure_directory_exists_group = augroup('EnsureDirectoryExists', { clear = true })
+autocmd('BufNewFile', {
+  group = ensure_directory_exists_group,
+  pattern = '*',
+  callback = function()
+    require('util.ensure_directory_exists').ensure_directory_exists()
+  end,
+})
 
-local last_cursor_group = vim.api.nvim_create_augroup('LastCursor', { clear = true })
-vim.api.nvim_create_autocmd('BufReadPost', {
-    group = last_cursor_group,
-    pattern = '*',
-    callback = function()
-      if vim.bo.filetype ~= 'gitcommit'
+local fast_escape_group = augroup('FastEscape', { clear = true })
+autocmd({ 'InsertEnter', 'CmdlineEnter' }, {
+  group = fast_escape_group,
+  pattern = '*',
+  command = 'set timeoutlen=200',
+})
+autocmd({ 'InsertLeave', 'CmdlineLeave' }, {
+  group = fast_escape_group,
+  pattern = '*',
+  command = 'set timeoutlen=1000',
+})
+
+local last_cursor_group = augroup('LastCursor', { clear = true })
+autocmd('BufReadPost', {
+  group = last_cursor_group,
+  pattern = '*',
+  callback = function()
+    if vim.bo.filetype ~= 'gitcommit'
         and vim.fn.line("'\"") > 0
         and vim.fn.line("'\"") <= vim.fn.line('$') then
-          vim.cmd('normal g`"')
-      end
-    end,
-  })
+      vim.cmd('normal g`"')
+    end
+  end,
+})
+
+local group = augroup('LastFileOnStartup', { clear = true })
+
+autocmd('VimEnter', {
+  group = group,
+  callback = function()
+    vim.defer_fn(function()
+      require('util.last_file_cwd').last_buffer()
+    end, 10)
+  end,
+})
