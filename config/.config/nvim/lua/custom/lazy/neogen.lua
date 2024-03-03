20c2c4e7f8b7e6c430250a30175c28ee2ba667f6
John Marks <jrmarks11@gmail.com>
2024-03-03 05:57:55 -0600

    Luagen

---
+++ config/.config/nvim/lua/custom/lazy/neogen.lua
@@ -0,0 +1,26 @@
+return {
+  "danymat/neogen",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "L3MON4D3/LuaSnip",
+  },
+  config = function()
+    local neogen = require("neogen")
+
+    neogen.setup({
+      snippet_engine = "luasnip"
+    })
+
+    local map = vim.keymap.set
+
+    map("n", "sf", function()
+      neogen.generate({ type = "func" })
+    end)
+
+    map("n", "st", function()
+      neogen.generate({ type = "type" })
+    end)
+  end,
+  -- Uncomment next line if you want to follow only stable versions
+  -- version = "*"
+}
