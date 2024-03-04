d8df31fc8ba654783d8a5a92735681e703282d0d
John Marks <jrmarks11@gmail.com>
2024-03-04 10:57:16 -0600

    Cleanup keybindings

--- config/.config/nvim/lua/custom/keybindings/all_editors.lua
+++ config/.config/nvim/lua/custom/keybindings/all_editors.lua
@@ -1,13 +1,13 @@
 local map = vim.keymap.set
 local cmd = vim.cmd
 
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
-map("n", "J", "mzJ`z")
-map("n", "<C-d>", "<C-d>zz")
-map("n", "<C-u>", "<C-u>zz")
-map("n", "n", "nzzzv")
-map("n", "N", "Nzzzv")
+map('v', 'J', ":m '>+1<CR>gv=gv")
+map('v', 'K', ":m '<-2<CR>gv=gv")
+map('n', 'J', 'mzJ`z')
+map('n', '<C-d>', '<C-d>zz')
+map('n', '<C-u>', '<C-u>zz')
+map('n', 'n', 'nzzzv')
+map('n', 'N', 'Nzzzv')
 
 map('n', "'", '`')
 map('x', "'", '`')
@@ -26,14 +26,12 @@ map('o', 'ae', function()
   cmd('keepjumps normal! ggVG')
 end, { silent = true })
 
-map('n', 'j', [[v:count ? (v:count > 5 ? "m'" . v:count : '') . 'j' : 'gj']], { expr = true })
-map('n', 'k', [[v:count ? (v:count > 5 ? "m'" . v:count : '') . 'k' : 'gk']], { expr = true })
-map('x', 'j', [[v:count ? (v:count > 5 ? "m'" . v:count : '') . 'j' : 'gj']], { expr = true })
-map('x', 'k', [[v:count ? (v:count > 5 ? "m'" . v:count : '') . 'k' : 'gk']], { expr = true })
+map({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 map('n', 'Q', '@q')
 map('x', 'Q', [[:'<,'>normal @q<cr>]])
 map('x', '.', [[:normal .<cr>]])
 
-map('n', 's', "<nop>", { remap = true })
+map('n', 's', '<nop>', { remap = true })
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
