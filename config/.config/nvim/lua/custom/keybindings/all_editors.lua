8a68b03f830abf5dd857bc341921d579ab993a59
John Marks <jrmarks11@gmail.com>
2024-03-04 12:52:30 -0600

    Cleanup keybindings

--- config/.config/nvim/lua/custom/keybindings/all_editors.lua
+++ config/.config/nvim/lua/custom/keybindings/all_editors.lua
@@ -3,24 +3,25 @@ local cmd = vim.cmd
 
 map('v', 'J', ":m '>+1<CR>gv=gv")
 map('v', 'K', ":m '<-2<CR>gv=gv")
+
 map('n', 'J', 'mzJ`z')
 map('n', '<C-d>', '<C-d>zz')
 map('n', '<C-u>', '<C-u>zz')
 map('n', 'n', 'nzzzv')
 map('n', 'N', 'Nzzzv')
 
-map('n', "'", '`')
-map('x', "'", '`')
-map('n', ';', ':')
-map('x', ';', ':')
+map({ 'n', 'x' }, "'", '`')
+map({ 'n', 'x' }, ';', ':')
+
 map('n', 'K', 'i<cr><esc>^mwgk:silent! s/\\v +$//<cr>:noh<cr>`w', { silent = true })
 
 map('n', '[<Space>', ':<C-u>put! =repeat(nr2char(10),v:count1)<CR>', { silent = true })
 map('n', ']<Space>', ':<C-u>put =repeat(nr2char(10),v:count1)<CR>', { silent = true })
+
 map('n', '<Space>p', '<cmd>lua require("util.no_paste").paste()<CR>', { silent = true })
 map('n', '<Space><Tab>', '<C-^>')
-map('x', 'ae', 'gg0oG$', { silent = true })
 
+map('x', 'ae', 'gg0oG$', { silent = true })
 map('o', 'ae', function()
   cmd('execute "normal! m`"')
   cmd('keepjumps normal! ggVG')
