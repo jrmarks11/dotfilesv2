28f915fce20587ec0c11913d22c1758214a7a94e
John Marks <jrmarks11@gmail.com>
2024-03-04 10:45:51 -0600

    Update plugins

--- config/.config/nvim/lua/custom/lazy/simple_plugins.lua
+++ config/.config/nvim/lua/custom/lazy/simple_plugins.lua
@@ -1,7 +1,7 @@
 return {
   'rhysd/clever-f.vim',
   {
-    "github/copilot.vim",
+    'github/copilot.vim',
     cond = vim.fn.exists('g:vscode') == 0
   },
 }
--- config/.config/nvim/lua/custom/lazy/trouble.lua
+++ config/.config/nvim/lua/custom/lazy/trouble.lua
@@ -1,21 +1,21 @@
 return {
   {
-    "folke/trouble.nvim",
+    'folke/trouble.nvim',
     config = function()
-      require("trouble").setup({
+      require('trouble').setup({
         icons = false,
       })
 
-      vim.keymap.set("n", ",q", function()
-        require("trouble").toggle()
+      vim.keymap.set('n', ',q', function()
+        require('trouble').toggle()
       end)
 
-      vim.keymap.set("n", "]q", function()
-        require("trouble").next({skip_groups = true, jump = true});
+      vim.keymap.set('n', ']q', function()
+        require('trouble').next({skip_groups = true, jump = true});
       end)
 
-      vim.keymap.set("n", "[q", function()
-        require("trouble").previous({skip_groups = true, jump = true});
+      vim.keymap.set('n', '[q', function()
+        require('trouble').previous({skip_groups = true, jump = true});
       end)
 
     end
