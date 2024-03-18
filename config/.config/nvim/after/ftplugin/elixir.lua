3a68bc720266bd2049925dbe7de500cdefc532aa
John Marks <jmarks@bitfreighter.com>
2024-03-18 13:51:22 -0500

    Add tracing to vim-test for elixir

--- config/.config/nvim/after/ftplugin/elixir.lua
+++ config/.config/nvim/after/ftplugin/elixir.lua
@@ -1,10 +1,12 @@
 local map = vim.keymap.set
 local opt = vim.opt
 
-map('n', ',g', '<cmd>lua require("util.elixir").run_debug_test()<CR>')
 map('n', ',f', '<cmd>TestFile --trace<cr>')
+map('n', ',g', '<cmd>lua require("util.elixir").run_debug_test()<CR>')
 map('n', ',i', '"zyawoIO.inspect(<C-r>z)<Esc>')
+map('n', ',k', '<cmd>TestLast --trace<cr>')
 map('n', ',p', 'Orequire IEx; IEx.pry()<Esc>')
+map('n', ',t', '<cmd>TestNearest --trace<cr>')
 map('n', ',y', ':g/^\\W*require IEx.*IEx.pry()$/d<CR>')
 
 opt.colorcolumn = { 101, 102 }
