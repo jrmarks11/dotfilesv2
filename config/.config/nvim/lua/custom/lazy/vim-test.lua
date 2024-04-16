f96b08bfec78f9e4649426bde933120cf3b1b8fa
John Marks <jmarks@bitfreighter.com>
2024-04-16 05:06:31 -0500

    Go back to tslime

--- config/.config/nvim/lua/custom/lazy/vim-test.lua
+++ config/.config/nvim/lua/custom/lazy/vim-test.lua
@@ -1,14 +1,16 @@
 return {
   'janko-m/vim-test',
+  dependencies = { 'jgdavey/tslime.vim' },
   event = { 'VeryLazy' },
 
   config = function()
     local g = vim.g
     local map = vim.keymap.set
 
-    g['test#strategy'] = 'neovim_sticky'
-    g['test#neovim#term_position'] = 'vertical'
     g['test#preserve_screen'] = 1
+    g['test#strategy'] = 'tslime'
+    g['tslime_always_current_session'] = 1
+    g['tslime_always_current_window'] = 1
 
     map('n', ',t', ':TestNearest<CR>')
     map('n', ',k', ':TestLast<CR>')
