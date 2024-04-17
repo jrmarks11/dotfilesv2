99e66ffcb895181c74e1731ebb55ff49d839d4e1
John Marks <jmarks@bitfreighter.com>
2024-04-17 15:09:57 -0500

    Add watch.test to vim-test

--- config/.config/nvim/lua/util/elixir.lua
+++ config/.config/nvim/lua/util/elixir.lua
@@ -10,4 +10,13 @@ function M.run_debug_test()
   g['test#elixir#exunit#executable'] = executable
 end
 
+function M.run_watch_test()
+  local g = vim.g
+
+  local executable = g['test#elixir#exunit#executable']
+  g['test#elixir#exunit#executable'] = 'mix test.watch'
+  vim.cmd('TestFile')
+  g['test#elixir#exunit#executable'] = executable
+end
+
 return M
