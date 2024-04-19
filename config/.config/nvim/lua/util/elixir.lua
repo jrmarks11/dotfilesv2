f52a3cbaac186ad46bb10163a611cda9f41873c5
John Marks <jmarks@bitfreighter.com>
2024-04-19 13:34:58 -0500

    Using tmux for testing instead of vim

--- config/.config/nvim/lua/util/elixir.lua
+++ config/.config/nvim/lua/util/elixir.lua
@@ -2,11 +2,12 @@ local M = {}
 
 function M.run_debug_test()
   local g = vim.g
+  local ntn = require'nvim-tmux-navigation'
 
   local executable = g['test#elixir#exunit#executable']
   g['test#elixir#exunit#executable'] = 'iex -S mix test'
   vim.cmd('TestNearest --trace')
-  vim.cmd('wincmd l')
+  ntn.NvimTmuxNavigateRight()
   g['test#elixir#exunit#executable'] = executable
 end
 
