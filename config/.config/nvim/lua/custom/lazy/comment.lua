4b7de9ed414d6aab29af48913e74a9095a4ad8ec
John Marks <jmarks@bitfreighter.com>
2024-04-22 09:04:08 -0500

    TS commentstring

--- config/.config/nvim/lua/custom/lazy/comment.lua
+++ config/.config/nvim/lua/custom/lazy/comment.lua
@@ -2,8 +2,20 @@ return {
   'numToStr/Comment.nvim',
   event = 'VeryLazy',
 
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    'nvim-treesitter/nvim-treesitter',
+  },
+
   config = function()
-    require('Comment').setup()
+    require('ts_context_commentstring').setup {
+      enable_autocmd = false,
+    }
+
+    local prehook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+    require('Comment').setup {
+      pre_hook = prehook
+    }
   end,
 
   cond = vim.fn.exists('g:vscode') == 0
