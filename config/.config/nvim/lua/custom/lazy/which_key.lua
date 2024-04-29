3aec60dc1e5dff8d45fd94d5ec3036eafba649bd
John Marks <jmarks@bitfreighter.com>
2024-04-29 08:56:59 -0500

    Dont need to register leader keys

--- config/.config/nvim/lua/custom/lazy/which_key.lua
+++ config/.config/nvim/lua/custom/lazy/which_key.lua
@@ -2,12 +2,6 @@ return {
   "folke/which-key.nvim",
   config = function()
     require("which-key").setup {}
-
-    require("which-key").register({
-      ["s"] = { name = "Mappings for 's'" },
-      [","] = { name = "Mappings for comma" },
-      ["<space>"] = { name = "Mappings for space" }
-    }, { mode = "n" })   -- Adjust the mode according to your needs
   end,
 
   cond = vim.fn.exists('g:vscode') == 0
