266118168517ae8753a2161989d69e62d8c03b50
John Marks <jrmarks11@gmail.com>
2024-05-26 22:16:39 -0500

    Maybe Nvim-lint

---
+++ config/.config/nvim/lua/custom/lazy/lint.lua
@@ -0,0 +1,22 @@
+return {}
+-- return {
+--   "mfussenegger/nvim-lint",
+--   config = function()
+--     local lint = require("lint")
+--     lint.linters_by_ft = {
+--       ruby = {'rubocop'},
+--       elixir = {'credo'},
+--       sh = { 'shellcheck', },
+--       bash = { 'shellcheck', },
+--       zsh = { 'shellcheck', },
+--       javascript = { 'eslint_d' },
+--       typescript = { 'eslint_d' },
+--     }
+--
+--     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+--       callback = function()
+--         require("lint").try_lint()
+--       end,
+--     })
+--   end
+-- }
