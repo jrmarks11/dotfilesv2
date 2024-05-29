1842055db7b60a48e275229e346c123daefe2004
John Marks <jrmarks11@gmail.com>
2024-05-28 21:38:22 -0500

    Credo is not strict

--- config/.config/nvim/lua/custom/lazy/lint.lua
+++ config/.config/nvim/lua/custom/lazy/lint.lua
@@ -2,6 +2,18 @@ return {
   'mfussenegger/nvim-lint',
   config = function()
     local lint = require 'lint'
+
+    local errorfmt = '[%t] %. stdin:%l:%c %m, [%t] %. stdin:%l %m'
+
+    lint.linters.credo = {
+      cmd = 'mix',
+      stdin = true,
+      args = { 'credo', 'list', '--format=oneline', '--read-from-stdin' },
+      stream = 'stdout',
+      ignore_exitcode = true,
+      parser = require('lint.parser').from_errorformat(errorfmt, { ['source'] = 'credo' }),
+    }
+
     lint.linters_by_ft = {
       elixir = { 'credo' },
     }
