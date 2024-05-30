5579779f8753bae8c785c6fc60f7c5a213530399
John Marks <jrmarks11@gmail.com>
2024-05-29 22:34:01 -0500

    Cleanup paste

--- config/.config/nvim/lua/util/no_paste.lua
+++ config/.config/nvim/lua/util/no_paste.lua
@@ -1,21 +1,10 @@
 local M = {}
 
 function M.paste()
-  local opt = vim.opt
   local cmd = vim.cmd
-
-  local original_paste = opt.paste
-  local original_expandtab = opt.expandtab
-
-  opt.paste = true
-  opt.expandtab = true
 
   cmd 'normal! o'
   cmd 'normal! ]p'
-
-  opt.paste = original_paste
-  opt.expandtab = original_expandtab
-
   cmd 'normal! `[V`]='
 end
 
