f84d703124f2998ec7892d402b3926e243154a6b
John Marks <jrmarks11@gmail.com>
2024-06-01 22:17:24 -0500

    Cleanup descriptions

--- config/.config/nvim/lua/custom/keybindings/telescope.lua
+++ config/.config/nvim/lua/custom/keybindings/telescope.lua
@@ -1,26 +1,37 @@
-local telescope = require('telescope')
+local telescope = require 'telescope'
 local map = vim.keymap.set
-local builtin = require('telescope.builtin')
+local builtin = require 'telescope.builtin'
 
-map('n', '<Space>B', builtin.buffers, { desc = 'Buffers' })
-map('n', '<Space>D', function() builtin.find_files({ cwd = '%:h' }) end, { desc = 'Find files in current directory' })
-map('n', '<Space>G', builtin.git_status, { desc = 'Git status files' })
-map('n', '<Space>R', function() telescope.extensions.recent_files.pick() end, { desc = 'Recent files' })
-map('n', '<Space>T', builtin.find_files, { desc = 'Find files' })
+map('n', '<Space>b', builtin.buffers, { desc = '[B]uffers' })
 
-map('n', '<Space>F', function()
-  local word = vim.fn.expand('<cword>')
-  builtin.grep_string({ search = word })
-end, { desc = 'Grep word under cursor' })
+map('n', '<Space>d', function()
+  builtin.find_files { cwd = '%:h' }
+end, { desc = 'Find in CW[D]' })
 
-map('x', '<Space>F', function()
-  vim.cmd('normal! y')
-  local visual_text = vim.fn.getreg('')
-  builtin.grep_string({ search = visual_text })
-end, { desc = 'Grep visual selection' })
+map('n', '<Space>g', builtin.git_status, { desc = '[G]it Status Files' })
 
-map('n', '<Space>H',  builtin.help_tags, { desc = 'Help tags' })
+map('n', '<Space>r', function()
+  telescope.extensions.recent_files.pick()
+end, { desc = '[R]ecent files' })
+map('n', '<Space>t', builtin.find_files, { desc = '[T]o Files' })
 
-map('n', '<Space>J', function()
-  builtin.grep_string({ search = vim.fn.input('grep ❯ ') })
-end, { desc = 'Grep' })
+map('n', '<Space>f', function()
+  local word = vim.fn.expand '<cword>'
+  builtin.grep_string { search = word }
+end, { desc = '[F]ind Word' })
+
+map('x', '<Space>f', function()
+  vim.cmd 'normal! y'
+  local visual_text = vim.fn.getreg ''
+  builtin.grep_string { search = visual_text }
+end, { desc = '[F]ind Visual Selection' })
+
+map('n', '<Space>h', builtin.help_tags, { desc = '[H]elp' })
+
+map('n', '<Space>j', function()
+  builtin.grep_string { search = vim.fn.input 'grep ❯ ' }
+end, { desc = 'Find in Pro[j]ect' })
+
+map('n', '<Space>u', function()
+  builtin.resume()
+end, { desc = 'Res[u]me' })
