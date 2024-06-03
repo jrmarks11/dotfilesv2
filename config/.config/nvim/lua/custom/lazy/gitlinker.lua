005e7d45c05ad3914acf81b2b015a2cef058a946
John Marks <jmarks@bitfreighter.com>
2024-06-03 16:18:33 -0500

    Better gitlinker mappings

--- config/.config/nvim/lua/custom/lazy/gitlinker.lua
+++ config/.config/nvim/lua/custom/lazy/gitlinker.lua
@@ -5,8 +5,16 @@ return {
 
   config = function()
     require('gitlinker').setup {
-      mappings = 'sy',
+      mappings = nil,
     }
+
+    vim.keymap.set('n', 'sh', function()
+      require('gitlinker').get_buf_range_url 'n'
+    end, { desc = 'Git[H]ub link for current buffer' })
+
+    vim.keymap.set('v', 'sh', function()
+      require('gitlinker').get_buf_range_url 'v'
+    end, { desc = 'Git[H]ub link for current buffer' })
   end,
 
   cond = vim.fn.exists 'g:vscode' == 0,
