cdbac6691a3953a52152b4a0f7d7557fd3943684
John Marks <jmarks@bitfreighter.com>
2024-06-03 16:07:48 -0500

    Remove unused keybindings

--- config/.config/nvim/lua/custom/lazy/mini.lua
+++ config/.config/nvim/lua/custom/lazy/mini.lua
@@ -4,7 +4,16 @@ return {
   event = 'VeryLazy',
 
   config = function()
-    require('mini.ai').setup()
+    require('mini.ai').setup {
+      mappings = {
+        around_next = '',
+        inside_next = '',
+        around_last = '',
+        inside_last = '',
+        goto_left = '',
+        goto_right = '',
+      },
+    }
 
     require('mini.indentscope').setup {
       draw = {
