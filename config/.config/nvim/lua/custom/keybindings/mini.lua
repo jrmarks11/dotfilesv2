2ff8cdf503d1563b04d9ae3e09d3c16bbceec675
John Marks <jmarks@bitfreighter.com>
2024-06-03 12:32:46 -0500

    Cleanup mappings

--- config/.config/nvim/lua/custom/keybindings/mini.lua
+++ config/.config/nvim/lua/custom/keybindings/mini.lua
@@ -1,20 +1,15 @@
 local map = vim.keymap.set
 
-local map_vis = function(keys, call, desc)
-  local rhs = '<Cmd>lua MiniVisits.' .. call .. '<CR>'
-  map('n', '<Leader>' .. keys, rhs, { desc = desc })
-end
-
-map_vis('m', 'select_path(nil, { filter = "marked" })', '[M]arked Files')
+map('n', '<leader>m', '<Cmd>lua MiniVisits.select_path(nil, { filter = "marked" })<CR>', { desc = '[M]arked Files' })
 map('n', '<leader><leader>', '<Cmd>lua require("util.visits").toggle_marked()<CR>', { desc = 'Toggle Marked' })
 
-local map_iterate_core = function(lhs, direction, desc)
+local map_iterate_marks = function(lhs, direction, desc)
   local opts = { filter = 'marked', wrap = true }
   local rhs = function()
-    MiniVisits.iterate_paths(direction, vim.fn.getcwd(), opts)
+    require('mini.visits').iterate_paths(direction, vim.fn.getcwd(), opts)
   end
   map('n', lhs, rhs, { desc = desc })
 end
 
-map_iterate_core('<leader>n', 'forward', '[N]ext Marked Label')
-map_iterate_core('<leader>p', 'backward', '[P]revious Marked Label')
+map_iterate_marks('<leader>n', 'forward', '[N]ext Marked Label')
+map_iterate_marks('<leader>p', 'backward', '[P]revious Marked Label')
