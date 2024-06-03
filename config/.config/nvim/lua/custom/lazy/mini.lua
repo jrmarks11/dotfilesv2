9944efb154be82c426809aee2eef2771b6bc6f37
John Marks <jmarks@bitfreighter.com>
2024-06-03 08:51:45 -0500

    Use Mini instead of modules individually

---
+++ config/.config/nvim/lua/custom/lazy/mini.lua
@@ -0,0 +1,16 @@
+return {
+  'echasnovski/mini.nvim',
+  version = false,
+  event = 'VeryLazy',
+
+  config = function()
+    require('mini.ai').setup()
+    require('mini.indentscope').setup()
+
+    require('mini.splitjoin').setup {
+      mappings = {
+        toggle = 'ss',
+      },
+    }
+  end,
+}
--- config/.config/nvim/lua/custom/lazy/splitjoin.lua
+++ config/.config/nvim/lua/custom/lazy/splitjoin.lua
@@ -1,22 +1,11 @@
 return {
   {
-    'echasnovski/mini.splitjoin',
-    version = '*',
-    event = 'VeryLazy',
-
-    config = function()
-      require('mini.splitjoin').setup {
-        mappings = {
-          toggle = 'ss',
-        },
-      }
-    end,
-  },
-  {
     'AndrewRadev/splitjoin.vim',
     event = 'VeryLazy',
 
     init = function()
+      vim.g.splitjoin_split_mapping = ''
+      vim.g.splitjoin_join_mapping = ''
       vim.keymap.set('n', 'sj', '<cmd>SplitjoinJoin<cr>', { desc = '[J]oin' })
       vim.keymap.set('n', 'sk', '<cmd>SplitjoinSplit<cr>', { desc = 'Split [K]' })
     end,
