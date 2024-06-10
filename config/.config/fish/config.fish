2083d775aade94841eec6900e5f8cf4c31901425
John Marks <jrmarks11@gmail.com>
2024-06-09 21:40:37 -0500

    Add brew to fish

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -1,6 +1,12 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+if test -d /opt/homebrew
+	set -gx HOMEBREW_PREFIX "/opt/homebrew"
+	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
 end
+
+fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
+! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
+! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
 
 zoxide init fish | source
 fzf --fish | source
