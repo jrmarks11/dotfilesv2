f7629375939e30e852bd21592c2a8069fbadc30a
John Marks <jmarks@bitfreighter.com>
2024-06-13 16:20:58 -0500

    Move cmp to its own config

--- config/.config/nvim/lua/custom/lazy/lsp.lua
+++ config/.config/nvim/lua/custom/lazy/lsp.lua
@@ -5,16 +5,10 @@ return {
   dependencies = {
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/nvim-cmp',
   },
 
   config = function()
     require 'custom.config.mason'
-    require 'custom.config.cmp'
     require 'custom.config.diagnostic'
   end,
 }
