5f1a9b8346dadbe43afe5139933e1af172570219
John Marks <jmarks@bitfreighter.com>
2024-06-13 22:19:54 -0500

    Fix dependencies

--- config/.config/nvim/lua/custom/lazy/cmp.lua
+++ config/.config/nvim/lua/custom/lazy/cmp.lua
@@ -3,12 +3,10 @@ return {
   event = { 'BufReadPre', 'BufNewFile' },
 
   dependencies = {
-    'L3MON4D3/LuaSnip',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
-    'vim-dadbod/completion',
   },
 
   config = function()
