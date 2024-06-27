a70ec92256313b3ca91c4ec7161896ff5abe3d94
John Marks <jmarks@bitfreighter.com>
2024-06-27 10:23:29 -0500

    Fix branch files action

--- config/.config/nvim/lua/util/branch_files.lua
+++ config/.config/nvim/lua/util/branch_files.lua
@@ -2,19 +2,31 @@ local M = {}
 
 function M.branch_files()
   local cwd = vim.fn.getcwd()
-  require('fzf-lua').git_files {
+  local fzf_lua = require 'fzf-lua'
+
+  fzf_lua.git_files {
     cmd = 'git diff --name-only master -- ' .. cwd,
     prompt = 'BranchFiles❯ ',
     cwd = cwd,
-    previewer = 'custom',
+    previewer = false,
     preview = {
       type = 'cmd',
       fn = function(items)
-        local file = require('fzf-lua').path.entry_to_file(items[1])
+        local file = fzf_lua.path.entry_to_file(items[1])
         return string.format(
           'cd $(git rev-parse --show-toplevel) && git diff --color=always master -- %s | delta',
           file.path
         )
+      end,
+    },
+    actions = {
+      ['default'] = function(selected)
+        local file = fzf_lua.path.entry_to_file(selected[1])
+        local git_root = fzf_lua.path.git_root()
+        local relative_path = cwd:gsub('^' .. vim.pesc(git_root) .. '/', '')
+        local edit_path = file.path:gsub('^' .. vim.pesc(relative_path) .. '/', '')
+
+        vim.cmd('edit ' .. edit_path)
       end,
     },
   }
