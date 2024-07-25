40f31efd63c568acae77f62543e9307f4c7ea4f1
John Marks <jrmarks11@gmail.com>
2024-07-24 21:28:55 -0500

    Update plugins

--- config/.config/nvim/after/ftplugin/elixir.lua
+++ config/.config/nvim/after/ftplugin/elixir.lua
@@ -1,9 +1,7 @@
 local map = vim.keymap.set
 local opt = vim.opt
 
-map('n', ',f', '<cmd>TestFile --trace<cr>', {
-  desc = 'Test File --trace'
-})
+map('n', ',f', '<cmd>TestFile --trace<cr>', {desc = 'Test File --trace'})
 map('n', ',g', [[<cmd>lua require('util.elixir').run_debug_test()<CR>]], { desc = 'Test Nearest with Debugger' })
 map('n', ',k', '<cmd>TestLast --trace<cr>', { desc = 'Test Last --trace' })
 map('n', ',p', 'Orequire IEx; IEx.pry()<Esc>', { desc = 'Insert IEx.pry()' })
