03474d78d539817d466dac189945cfde0008b05d
John Marks <jmarks@bitfreighter.com>
2024-08-01 13:53:39 -0500

    Try gutentags

--- config/.config/nvim/lua/custom/autocmds/lsp.lua
+++ config/.config/nvim/lua/custom/autocmds/lsp.lua
@@ -16,29 +16,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     map(',r', vim.lsp.buf.rename, 'Rename')
     map('<C-k>', vim.lsp.buf.hover, 'Hover Documentation')
 
-    -- local client = vim.lsp.get_client_by_id(event.data.client_id)
-    -- if client and client.server_capabilities.documentHighlightProvider then
-    --   local highlight_augroup = vim.api.nvim_create_augroup('jmarks-lsp-highlight', { clear = false })
-    --   vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-    --     buffer = event.buf,
-    --     group = highlight_augroup,
-    --     callback = vim.lsp.buf.document_highlight,
-    --   })
-    --
-    --   vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-    --     buffer = event.buf,
-    --     group = highlight_augroup,
-    --     callback = vim.lsp.buf.clear_references,
-    --   })
-    --
-    --   vim.api.nvim_create_autocmd('LspDetach', {
-    --     group = vim.api.nvim_create_augroup('jmarks-lsp-detach', { clear = true }),
-    --     callback = function(event2)
-    --       vim.lsp.buf.clear_references()
-    --       vim.api.nvim_clear_autocmds { group = 'jmarks-lsp-highlight', buffer = event2.buf }
-    --     end,
-    --   })
-    -- end
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
 
     if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
       map(',h', function()
