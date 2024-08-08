ee956e4a1eca500c7a95b580184e33a527ca01d2
John Marks <jrmarks11@gmail.com>
2024-08-07 22:16:28 -0500

    Oil and Quicker

--- config/.config/nvim/lua/custom/lazy/oil.lua
+++ config/.config/nvim/lua/custom/lazy/oil.lua
@@ -5,9 +5,13 @@ return {
 
     config = function()
       require('oil').setup {
-        columns = { 'icon' },
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
         view_options = {
           show_hidden = true,
+          is_always_hidden = function(name)
+            return name == '..'
+          end,
         },
       }
 
