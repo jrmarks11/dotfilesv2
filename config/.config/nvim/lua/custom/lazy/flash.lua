a06638c8f1de075dfbf4fb3c8d9b590972f563f8
John Marks <jrmarks11@gmail.com>
2024-08-24 21:04:49 -0500

    Remove unused flash keys

--- config/.config/nvim/lua/custom/lazy/flash.lua
+++ config/.config/nvim/lua/custom/lazy/flash.lua
@@ -11,22 +11,7 @@ return {
       end,
       desc = 'Flash Search',
     },
-    {
-      's',
-      mode = {'o'},
-      function()
-        require('flash').jump()
-      end,
-      desc = 'Flash Search',
-    },
-    {
-      'S',
-      mode = { 'n', 'x', 'o' },
-      function()
-        require('flash').treesitter()
-      end,
-      desc = 'Flash Treesitter Search',
-    },
+
     {
       'r',
       mode = 'o',
@@ -34,22 +19,6 @@ return {
         require('flash').remote()
       end,
       desc = 'Remote Flash Search',
-    },
-    {
-      'R',
-      mode = { 'o', 'x' },
-      function()
-        require('flash').treesitter_search()
-      end,
-      desc = 'Treesitter Search',
-    },
-    {
-      '<c-s>',
-      mode = { 'c' },
-      function()
-        require('flash').toggle()
-      end,
-      desc = 'Toggle Flash Search',
     },
   },
 }
