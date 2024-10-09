7c41a6d463fffff1ae3457a8987981091275d820
John Marks <jmarks@bitfreighter.com>
2024-10-09 08:51:37 -0500

    Remove avante flash and undotree

--- config/.config/nvim/lua/custom/lazy/undotree.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'mbbill/undotree',
-  event = { 'BufReadPre' },
-
-  config = function()
-    local map = vim.keymap.set
-    map('n', ',u', ':UndotreeToggle<CR>', { desc = 'Toggle Undo Tree' })
-  end,
-}
