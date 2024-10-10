c162357e632a3a1e95840a857548aa03543c263f
John Marks <jmarks@bitfreighter.com>
2024-10-10 16:46:26 -0500

    Cleanup

--- config/.config/nvim/lua/custom/keybindings/fzf.lua
+++ config/.config/nvim/lua/custom/keybindings/fzf.lua
@@ -3,13 +3,9 @@ local function map(mode, key, command, description)
 end
 
 map('n', '<space>b', [[<cmd>lua require('util.branch_files').branch_files()<cr>]], 'Branch Files')
-map('n', '<space>d', [[<cmd>lua require('fzf-lua').files({cwd = '%:h'})<CR>]], 'Directory Files')
 map('n', '<space>f', [[<cmd>lua require('fzf-lua').grep_cword()<CR>]], 'Search Find Word')
 map('x', '<space>f', [[<cmd>lua require('fzf-lua').grep_visual()<CR>]], 'Search Find Visual')
 map('n', '<space>g', [[<cmd>lua require('fzf-lua').git_status()<CR>]], 'Git Status')
-map('n', '<space>h', [[<cmd>lua require('fzf-lua').help_tags()<CR>]], 'Search Help Docs')
-map('n', '<space>i', [[<cmd>lua require('fzf-lua').grep_curbuf()<CR>]], 'Search Grep Buffer')
 map('n', '<space>j', [[<cmd>lua require('fzf-lua').grep()<CR>]], 'Search Grep Project')
 map('n', '<space>t', [[<cmd>lua require('fzf-lua').files()<CR>]], 'To Files')
-map('n', '<space>T', [[<cmd>lua require('fzf-lua').tags()<CR>]], 'To Tags')
 map('n', '<space>u', [[<cmd>lua require('fzf-lua').resume()<CR>]], 'Resume Fzf Search')
--- config/.config/nvim/lua/custom/lazy/quicker.lua
+++ config/.config/nvim/lua/custom/lazy/quicker.lua
@@ -3,23 +3,6 @@ return {
   event = 'VeryLazy',
 
   config = function()
-    require('quicker').setup {
-      keys = {
-        {
-          '>',
-          function()
-            require('quicker').expand { before = 2, after = 2, add_to_existing = true }
-          end,
-          desc = 'Expand quickfix context',
-        },
-        {
-          '<',
-          function()
-            require('quicker').collapse()
-          end,
-          desc = 'Collapse quickfix context',
-        },
-      },
-    }
+    require('quicker').setup()
   end,
 }
