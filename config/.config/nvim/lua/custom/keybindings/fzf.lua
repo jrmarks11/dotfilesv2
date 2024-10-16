5d3cfb0e8a3c1b1a8e998a1fc778b4d19518bf7b
John Marks <jmarks@bitfreighter.com>
2024-10-16 14:48:44 -0500

    Add in grep for current buffer

--- config/.config/nvim/lua/custom/keybindings/fzf.lua
+++ config/.config/nvim/lua/custom/keybindings/fzf.lua
@@ -6,6 +6,7 @@ map('n', '<space>b', [[<cmd>lua require('util.branch_files').branch_files()<cr>]
 map('n', '<space>f', [[<cmd>lua require('fzf-lua').grep_cword()<CR>]], 'Search Find Word')
 map('x', '<space>f', [[<cmd>lua require('fzf-lua').grep_visual()<CR>]], 'Search Find Visual')
 map('n', '<space>g', [[<cmd>lua require('fzf-lua').git_status()<CR>]], 'Git Status')
+map('n', '<space>i', [[<cmd>lua require('fzf-lua').grep_curbuf()<CR>]], 'Search Current Buffer')
 map('n', '<space>j', [[<cmd>lua require('fzf-lua').grep({no_esc = true})<CR>]], 'Search Grep Project')
 map('n', '<space>t', [[<cmd>lua require('fzf-lua').files()<CR>]], 'To Files')
 map('n', '<space>u', [[<cmd>lua require('fzf-lua').resume()<CR>]], 'Resume Fzf Search')
