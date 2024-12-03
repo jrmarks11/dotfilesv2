c6bc39e93512edeb861d94e6c568665c0fd83c02
John Marks <jrmarks11@gmail.com>
2024-12-02 22:26:31 -0600

    Use more up to date gitlinker fork

--- config/.config/nvim/lua/custom/lazy/gitlinker.lua
+++ config/.config/nvim/lua/custom/lazy/gitlinker.lua
@@ -1,11 +1,13 @@
 return {
-  'ruifm/gitlinker.nvim',
-  event = { 'VeryLazy' },
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  'linrongbin16/gitlinker.nvim',
+  event = 'VeryLazy',
 
-  config = function()
-    require('gitlinker').setup { mappings = 'sh' }
-  end,
+  cmd = 'GitLink',
+  opts = {},
+  keys = {
+    { 'sh', '<cmd>GitLink<cr>', mode = { 'n', 'v' }, desc = 'Yank git link' },
+    { 'sH', '<cmd>GitLink!<cr>', mode = { 'n', 'v' }, desc = 'Open git link' },
+  },
 
   cond = vim.fn.exists 'g:vscode' == 0,
 }
