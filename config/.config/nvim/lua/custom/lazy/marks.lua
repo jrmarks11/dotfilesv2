0a5c38831822c0536b2545018e498a07faaf6f3b
John Marks <jmarks@bitfreighter.com>
2025-01-07 10:11:40 -0600

    Cleanup comments

--- config/.config/nvim/lua/custom/lazy/marks.lua
+++ config/.config/nvim/lua/custom/lazy/marks.lua
@@ -1,7 +1,7 @@
 return {
   'chentoast/marks.nvim',
   event = 'VeryLazy',
-  opts = {}, -- Pass plugin options here
+  opts = {},
   keys = {
     { 'n', ']m', '<Plug>(Marks-next)zz', desc = 'Jump to next mark' },
     { 'n', '[m', '<Plug>(Marks-prev)zz', desc = 'Jump to previous mark' },
