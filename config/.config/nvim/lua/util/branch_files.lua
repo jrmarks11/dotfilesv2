f35701236673d5a786b75e971c990a3bf7c0b74c
John Marks <jrmarks11@gmail.com>
2025-01-26 22:17:03 -0600

    fzf wip

--- config/.config/nvim/lua/util/branch_files.lua
+++ config/.config/nvim/lua/util/branch_files.lua
@@ -32,4 +32,34 @@ function M.branch_files()
   }
 end
 
+function M.bf2()
+  local cwd = vim.fn.getcwd()
+  local fzf_lua = require 'fzf-lua'
+  local actions = require 'fzf-lua.actions'
+
+  fzf_lua.git_files {
+    cmd = 'git diff --name-only master -- ' .. cwd,
+    prompt = 'BranchFiles❯ ',
+    cwd = cwd,
+    previewer = false,
+    preview = {
+      type = 'cmd',
+      fn = function(items)
+        local file = fzf_lua.path.entry_to_file(items[1])
+        return string.format(
+          'cd $(git rev-parse --show-toplevel) && git diff --color=always master -- %s | delta',
+          file.path
+        )
+      end,
+    },
+    actions = {
+      ['default'] = actions.file_edit,
+      ['ctrl-s'] = actions.file_split,
+      ['ctrl-v'] = actions.file_vsplit,
+      ['ctrl-t'] = actions.file_tabedit,
+      ['ctrl-q'] = actions.file_sel_to_qf,
+    },
+  }
+end
+
 return M
