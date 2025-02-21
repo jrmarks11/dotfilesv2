e8fd736ae3424ca0864af1a5410e67388ed827df
John Marks <jmarks@bitfreighter.com>
2025-02-21 15:28:31 -0600

    Lazyvim expirmenting

---
+++ config/.config/lazyvim/lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+return {
+  { "folke/trouble.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "nvim-lualine/lualine.nvim", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "MagicDuck/grug-far.nvim", enabled = false },
+  { "folke/lazydev.nvim", enabled = false },
+}
---
+++ config/.config/lazyvim/lua/plugins/tmux.lua
@@ -0,0 +1,12 @@
+return {
+  "alexghergh/nvim-tmux-navigation",
+  lazy = false,
+  opts = {},
+
+  keys = {
+    { "<C-j>h", "<cmd>NvimTmuxNavigateLeft<cr>", mode = { "n", "i", "c", "x", "t" }, desc = "Navigate Left" },
+    { "<C-j>j", "<cmd>NvimTmuxNavigateDown<cr>", mode = { "n", "i", "c", "x", "t" }, desc = "Navigate Down" },
+    { "<C-j>k", "<cmd>NvimTmuxNavigateUp<cr>", mode = { "n", "i", "c", "x", "t" }, desc = "Navigate Up" },
+    { "<C-j>l", "<cmd>NvimTmuxNavigateRight<cr>", mode = { "n", "i", "c", "x", "t" }, desc = "Navigate Right" },
+  },
+}
