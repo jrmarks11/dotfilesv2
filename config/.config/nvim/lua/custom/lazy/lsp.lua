184ceb6bb09b18c6bb12fab7d0bcb937cdaa1537
John Marks <jmarks@bitfreighter.com>
2025-02-27 15:35:38 -0600

    Add capablities to lsp

--- config/.config/nvim/lua/custom/lazy/lsp.lua
+++ config/.config/nvim/lua/custom/lazy/lsp.lua
@@ -4,6 +4,13 @@ return {
   dependencies = { 'williamboman/mason.nvim', 'williamboman/mason-lspconfig.nvim' },
 
   config = function()
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
+    local lspconfig = require 'lspconfig'
+
+    lspconfig['lua_ls'].setup { capabilities = capabilities }
+    lspconfig['ts_ls'].setup { capabilities = capabilities }
+    lspconfig['elixirls'].setup { capabilities = capabilities }
+
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('jmarks-lsp-attach', { clear = true }),
       callback = function(event)
