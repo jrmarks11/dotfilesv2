6e99cc0f547d6c25c1947e12254f5601fff8f955
John Marks <jmarks@bitfreighter.com>
2025-02-28 15:57:13 -0600

    Add a few more plugins to lazyvim

---
+++ config/.config/lazyvim/lua/plugins/vim-test.lua
@@ -0,0 +1,23 @@
+return {
+  "janko-m/vim-test",
+  dependencies = { { "preservim/vimux", cond = vim.fn.exists("g:vscode") == 0 } },
+  keys = {
+    { ",t", ":TestNearest<CR>", desc = "Test Nearest", mode = { "n", "x" } },
+    { ",k", ":TestLast<CR>", desc = "Test Last", mode = { "n", "x" } },
+    { ",f", ":TestFile<CR>", desc = "Test File", mode = { "n", "x" } },
+    { ",s", ":TestSuite<CR>", desc = "Test Suite", mode = { "n", "x" } },
+    { ",v", ":TestVisit<CR>zz", desc = "Test Visit", mode = { "n", "x" } },
+  },
+  ft = { "elixir" },
+
+  init = function()
+    local g = vim.g
+
+    if vim.g.vscode then
+      g["test#strategy"] = "neovim_vscode"
+    else
+      g["test#preserve_screen"] = 1
+      g["test#strategy"] = "vimux"
+    end
+  end,
+}
---
+++ config/.config/lazyvim/lua/plugins/word_motion.lua
@@ -0,0 +1,12 @@
+return {
+  "chaoren/vim-wordmotion",
+  keys = {
+    { ",w", "<Plug>WordMotion_w", desc = "Word Motion w", mode = { "n", "x", "o" } },
+    { ",b", "<Plug>WordMotion_b", desc = "Word Motion b", mode = { "n", "x", "o" } },
+    { ",e", "<Plug>WordMotion_e", desc = "Word Motion e", mode = { "n", "x", "o" } },
+  },
+
+  init = function()
+    vim.g.wordmotion_nomap = 1
+  end,
+}
