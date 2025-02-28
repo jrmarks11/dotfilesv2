c4419c5a150354339b0ddad634f7c9470077746e
John Marks <jrmarks11@gmail.com>
2025-02-27 23:14:11 -0600

    More plugins

--- config/.config/lazyvim/lua/plugins/mini.lua
+++ config/.config/lazyvim/lua/plugins/mini.lua
@@ -35,6 +35,25 @@ return {
     end,
   },
   {
+    "echasnovski/mini.surround",
+    version = false,
+    keys = {
+      { "sa", mode = { "n", "x" } },
+      { "sd", mode = { "n", "x" } },
+      { "sr", mode = { "n", "x" } },
+    },
+
+    opts = {
+      mappings = { find = "", find_left = "", highlight = "", update_n_lines = "", suffix_last = "", suffix_next = "" },
+    },
+  },
+  {
+    "echasnovski/mini.splitjoin",
+    version = false,
+    keys = { "ss" },
+    opts = { mappings = { toggle = "ss" } },
+  },
+  {
     "echasnovski/mini.statusline",
     version = false,
     event = "VeryLazy",
---
+++ config/.config/lazyvim/lua/plugins/which-key.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/which-key.nvim",
+  event = { "VeryLazy" },
+
+  opts = {
+    triggers = {
+      { "<auto>", mode = "nixsotc" },
+      { "s", mode = { "n", "v" } },
+    },
+  },
+}
