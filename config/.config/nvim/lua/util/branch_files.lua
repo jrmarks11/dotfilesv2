0835d578a6d8bfc1ddc7c2673fde5b7b622f9dcb
John Marks <jmarks@bitfreighter.com>
2025-11-12 15:41:53 -0600

    Update branch picker

--- config/.config/nvim/lua/util/branch_files.lua
+++ config/.config/nvim/lua/util/branch_files.lua
@@ -1,37 +1,22 @@
 local M = {}
 
-function M.branch_files()
-  local root = vim.fn.systemlist('git rev-parse --show-toplevel')[1]
-  local fzf_lua = require 'fzf-lua'
-  local actions = require 'fzf-lua.actions'
-
-  fzf_lua.fzf_exec('git diff --name-only master --relative', {
-    prompt = 'BranchFiles❯ ',
-    cwd = root,
-    fzf_opts = { ['--multi'] = '' },
-    preview = {
-      type = 'cmd',
-      fn = function(items)
-        local file = fzf_lua.path.entry_to_file(items[1])
-        return string.format('cd %s && git diff --color=always master -- %s', root, file.path)
-      end,
-    },
-    actions = {
-      ['default'] = actions.file_edit_or_qf,
-    },
-  })
-end
-
 function M.snacks_branch_files()
   Snacks.picker.pick {
     finder = function()
       local output = vim.fn.systemlist 'git diff --name-only origin/master --relative'
       local items = {}
       for _, file in ipairs(output) do
-        table.insert(items, { text = file, file = file })
+        local diff_output = vim.fn.system('git diff origin/master -- ' .. vim.fn.shellescape(file))
+        table.insert(items, {
+          text = file,
+          file = file,
+          diff = diff_output,
+        })
       end
       return items
     end,
+    format = "git_status",
+    preview = "diff",
   }
 end
 
