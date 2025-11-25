5a3903c09b0dff3a20d04c478e6c52ef71e661ed
John Marks <jmarks@bitfreighter.com>
2025-11-25 10:43:59 -0600

    Clean up zsh config

--- zsh/.zsh-files/functions.zsh
+++ zsh/.zsh-files/functions.zsh
@@ -17,7 +17,7 @@ not_bf_master() {
 
     if [[ $CURRENTBRANCH == "master" ]]; then
         echo "You are on master you donkey!"
-        1 > /dev/null 2>&1
+        return 1
     fi
 }
 
@@ -62,20 +62,6 @@ gc() {
 gush() {
     is_in_git_repo && not_bf_master || return
     git push
-}
-
-tm() {
-    [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
-    if [ "$1" ]; then
-        tmux $change -t "$1" 2>/dev/null ||
-        (tmux new-session -d -s "$1" && tmux $change -t "$1"); return
-    fi
-    if (( $(tmux list-sessions | wc -l) == 1 )); then
-        session=$(tmux list-sessions -F "#{session_name}")
-    else
-        session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0)
-    fi
-    tmux $change -t "$session" || tm "$(whoami)"
 }
 
 gdl() {
