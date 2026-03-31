449f61e14b27b8220cf61deb1cd49b6b35cf2199
John Marks <jmarks@bitfreighter.com>
2026-03-31 10:15:55 -0500

    New treesitter

--- config/.config/nvim/lua/custom/lazy/treesitter.lua
+++ config/.config/nvim/lua/custom/lazy/treesitter.lua
@@ -1,15 +1,12 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  event = { 'BufReadPost', 'BufNewFile' },
-  dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
-  build = ':TSUpdate',
+  {
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
+    build = ':TSUpdate',
 
-  config = function()
-    require('nvim-treesitter.configs').setup {
-      modules = {},
-      sync_install = false,
-      ignore_install = {},
-      ensure_installed = {
+    config = function()
+      local wanted = {
         'css',
         'dockerfile',
         'elixir',
@@ -27,21 +24,55 @@ return {
         'vim',
         'yaml',
         'comment',
-      },
+      }
 
-      highlight = { enable = true },
-      auto_install = true,
-      indent = { enable = true },
+      local installed = require('nvim-treesitter').get_installed()
+      local installed_set = {}
+      for _, lang in ipairs(installed) do
+        installed_set[lang] = true
+      end
 
-      textobjects = {
-        move = {
-          enable = true,
-          goto_next_start = { [']f'] = '@function.outer' },
-          goto_next_end = { [']F'] = '@function.outer' },
-          goto_previous_start = { ['[f'] = '@function.outer' },
-          goto_previous_end = { ['[F'] = '@function.outer' },
-        },
-      },
-    }
-  end,
+      local missing = {}
+      for _, lang in ipairs(wanted) do
+        if not installed_set[lang] then
+          table.insert(missing, lang)
+        end
+      end
+
+      if #missing > 0 then
+        require('nvim-treesitter').install(missing)
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          if pcall(vim.treesitter.start, args.buf) then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
+    event = { 'BufReadPost', 'BufNewFile' },
+
+    config = function()
+      local move = require('nvim-treesitter-textobjects.move')
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']f', function()
+        move.goto_next_start('@function.outer', 'textobjects')
+      end)
+      vim.keymap.set({ 'n', 'x', 'o' }, ']F', function()
+        move.goto_next_end('@function.outer', 'textobjects')
+      end)
+      vim.keymap.set({ 'n', 'x', 'o' }, '[f', function()
+        move.goto_previous_start('@function.outer', 'textobjects')
+      end)
+      vim.keymap.set({ 'n', 'x', 'o' }, '[F', function()
+        move.goto_previous_end('@function.outer', 'textobjects')
+      end)
+    end,
+  },
 }
